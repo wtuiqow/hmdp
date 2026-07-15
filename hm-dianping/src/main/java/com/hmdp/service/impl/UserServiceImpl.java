5b81377eeb4ce2f5da99ecb870876c8b5d720139
hhh_git <2578192171@qq.com>
2026-07-15 22:57:13 +0800

    拦截器配置

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -4,11 +4,14 @@ import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import cn.hutool.core.bean.BeanUtil;
 
 
 import javax.servlet.http.HttpSession;
@@ -68,7 +71,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             user =  createUserWithPhone(phone);
         }
         //7.保存用户信息到session中
-        session.setAttribute("user",user);
+        UserDTO userDTO = BeanUtil.copyProperties(user,UserDTO.class);
+        session.setAttribute("user",userDTO);
 
         return Result.ok();
     }
