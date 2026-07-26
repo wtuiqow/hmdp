6542f78e2b54fe20cc79bf4ebec6c0801ac356fa
hhh_git <2578192171@qq.com>
2026-07-26 19:59:41 +0800

    秒杀券下单

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +27,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional      //事务
+    public Result seckillVoucher(Long voucherId) {
+        // 1.查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2.判断秒杀是否开始
+        if(LocalDateTime.now().isBefore(voucher.getBeginTime())){
+            return Result.fail("活动未开始");
+        }
+        // 3.判断秒杀是否已经结束
+        if(LocalDateTime.now().isAfter(voucher.getEndTime())){
+            return Result.fail("活动已结束");
+        }
+        // 4.判断库存是否充足
+        if(voucher.getStock() < 1){
+            return Result.fail("卖完了");
+        }
+        //5，扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock= stock -1").eq("voucher_id", voucherId)
+                .update();
+        if(!success){
+            return Result.fail("卖完了");
+        }
+        //6.创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 6.1.订单id
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        // 6.2.用户id
+        Long userId = UserHolder.getUser().getId();     //`UserHolder` 是一个**基于 ThreadLocal 封装的工具类**，用来在同一**一次请求链路**里，随时随地获取当前登录用户信息
+        voucherOrder.setUserId(userId);
+        // 6.3.代金券id
+        voucherOrder.setVoucherId(voucherId);
+
+        save(voucherOrder);
+
+        return Result.ok(orderId);
+    }
 }
