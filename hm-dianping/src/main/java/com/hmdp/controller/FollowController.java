81a3b2ab05e43541c5a260cc0bc03d0c303bfe8c
hhh_git <2578192171@qq.com>
2026-08-08 00:27:34 +0800

    关注跟取消关注

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,5 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    @Autowired
+    private IFollowService followService;
+
+    @PutMapping("/{followedUserId}/{isFollow}")
+    public Result follow(@PathVariable Long followedUserId, @PathVariable boolean isFollow) {
+        return followService.follow(followedUserId,isFollow);
+    }
+
+    @GetMapping("/or/not/{followedUserId}")
+    public Result notFollow(@PathVariable Long followedUserId) {
+        return followService.followOrNot(followedUserId);
+    }
 
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +25,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result follow(Long followedUserId, boolean isFollow) {
+        UserDTO user = UserHolder.getUser();
+        Long userId = user.getId();
+        //true，关注
+        if(isFollow == true){
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followedUserId);
+            this.save(follow);
+        }else{
+            //delete from tb_follow where user_id = ? and follow_user_id = ?
+            remove(new QueryWrapper<Follow>()
+            .eq("user_id", userId).eq("follow_user_id",followedUserId));
+        }
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result followOrNot(Long followedUserId) {
+        UserDTO user = UserHolder.getUser();
+        Long userId = user.getId();
+
+        int count = query().eq("user_id", userId).eq("follow_user_id", followedUserId).count();
+        return Result.ok(count > 0);
+    }
 }
